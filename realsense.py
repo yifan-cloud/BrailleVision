9d6e4dab0893a0794c8afa34fe72e38eb2430052
Daniel Aaron <danjaaron@gmail.com>
2019-11-19 20:39:01 -0800

    rs depth map

--- realsense.py
+++ realsense.py
@@ -1,9 +1,13 @@
 ''' Source: https://github.com/IntelRealSense/librealsense/blob/jupyter/notebooks/depth_filters.ipynb
 '''
 
-import numpy as np                        # fundamental package for scientific computing
-import matplotlib.pyplot as plt           # 2D plotting library producing publication quality figures
-import pyrealsense2 as rs                 # Intel RealSense cross-platform open-source API
+# fundamental package for scientific computing
+import numpy as np
+# 2D plotting library producing publication quality figures
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+# Intel RealSense cross-platform open-source API
+import pyrealsense2 as rs
 print("Environment Ready")
 
 ''' Getting the data
@@ -15,32 +19,73 @@ cfg = rs.config()
 cfg.enable_device_from_file("./bags/stairs.bag")
 profile = pipe.start(cfg)
 
-# Skip 5 first frames to give the Auto-Exposure time to adjust
-for x in range(5):
-  pipe.wait_for_frames()
-  
-# Store next frameset for later processing:
-frameset = pipe.wait_for_frames()
-depth_frame = frameset.get_depth_frame()
+frames = []
+for x in range(10):
+    frameset = pipe.wait_for_frames()
+    frames.append(frameset.get_depth_frame())
 
-# Cleanup:
 pipe.stop()
 print("Frames Captured")
 
-
-''' Visualizing the data
-'''
-
 colorizer = rs.colorizer()
-colorized_depth = np.asanyarray(colorizer.colorize(depth_frame).get_data())
 
-plt.rcParams["axes.grid"] = False
-plt.rcParams['figure.figsize'] = [8, 4]
+depth_to_disparity = rs.disparity_transform(True)
+disparity_to_depth = rs.disparity_transform(False)
+
+decimation = rs.decimation_filter()
+decimation.set_option(rs.option.filter_magnitude, 4)
+
+spatial = rs.spatial_filter()
+spatial.set_option(rs.option.holes_fill, 3)
+
+temporal = rs.temporal_filter()
+
+hole_filling = rs.hole_filling_filter()
+
+
+
+for x in range(10):
+    frame = frames[x]
+    frame = decimation.process(frame)
+    frame = depth_to_disparity.process(frame)
+    frame = spatial.process(frame)
+    # frame = temporal.process(frame)
+    frame = disparity_to_depth.process(frame)
+    # frame = hole_filling.process(frame)
+
+colorized_depth = np.asanyarray(colorizer.colorize(frame).get_data())
 plt.imshow(colorized_depth)
 plt.show()
 
-''' Applying Filters 
+depth_frame = frame
+
+
+# downsample
+# bin colorized depth into 16 equal partitions (later -- user configure width and depth)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+xs, ys, zs = list(), list(), list()
+# plot_frames = list()
+darr = np.asanyarray(depth_frame.get_data())
+x_bin = darr.shape[0] // 4
+y_bin = darr.shape[1] // 4
+y_max = darr.shape[1]
+for x_idx in range(4):
+    x_frame = darr[x_bin*x_idx : x_bin*(x_idx+1)]
+    for y_idx in range(4):
+        frame = x_frame[:, y_bin*y_idx : y_bin*(y_idx+1)]
+        avg_depth = np.mean(frame)
+        print(x_idx, y_idx, avg_depth)
+        # plot_frames.append((x_idx, y_idx, avg_depth)) # (row, col, avg_depth)
+        xs.append(x_idx)
+        ys.append(y_idx)
+        zs.append(avg_depth)
+
+ax.scatter(xs, ys, zs)
+plt.show()
+
 '''
+# app filters
 
 # Decimation - reduces spatial resolution and adds hole filling
 
@@ -102,5 +147,4 @@ for x in range(10):
 colorized_depth = np.asanyarray(colorizer.colorize(frame).get_data())
 plt.imshow(colorized_depth)
 plt.show()
-
-
+'''
