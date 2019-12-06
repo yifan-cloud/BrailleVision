0c28a919355176dee98b1c55c3f412d8708150bd
melonehco <mzmao@colby.edu>
2019-12-06 10:29:24 -0800

    wrote code to get C array from numpy array

--- call_python.c
+++ call_python.c
@@ -1,6 +1,47 @@
 #include <Python.h>
 #include <numpy/arrayobject.h>
 
+double* py_array_to_c(PyArrayObject *in_array)
+{
+    NpyIter *in_iter;
+    NpyIter_IterNextFunc *in_iternext;
+
+    /*  create the iterator */
+    in_iter = NpyIter_New(in_array, NPY_ITER_READONLY, NPY_KEEPORDER,
+                             NPY_NO_CASTING, NULL);
+    if (in_iter == NULL)
+        goto fail;
+    
+    in_iternext = NpyIter_GetIterNext(in_iter, NULL);
+    if (in_iternext == NULL) {
+        NpyIter_Deallocate(in_iter);
+        goto fail;
+    }
+
+    double ** in_dataptr = (double **) NpyIter_GetDataPtrArray(in_iter);
+
+    npy_intp *shape = PyArray_SHAPE(in_array);
+    int len = shape[0] * shape[1] * shape[2];
+    double* out_array = (double*) malloc(sizeof(double) * len);
+
+    //TODO: figure out how to pass on array shape
+    /*  iterate over the array */
+    int idx = 0;
+    do {
+        out_array[idx] = **in_dataptr;
+        idx++;
+    } while(in_iternext(in_iter));
+
+    NpyIter_Deallocate(in_iter);
+
+    return out_array;
+
+    /*  in case bad things happen */
+    fail:
+        Py_XDECREF(out_array);
+        return NULL;
+}
+
 int main(int argc, char *argv[])
 {
     PyObject *pName, *pModule, *pFunc;
@@ -8,9 +49,42 @@ int main(int argc, char *argv[])
     int i;
     //pShape is for array shape
 
+    /*
+    the google vision code is in python too.
+    but probably can't run both at the same time
+    so I think one should call the other, but
+    hmm. there are the three modes. all of them need the realsense data
+    so really getting the array should be a function
+    and then all of them call that function. actually only the depth really needs the binned array
+    the others need just api calls
+    so they should just call single functions in python and get the resulting... strings?
+    that depends on how text-to-speech works. this should get whatever that outputs,
+    for the modes that say something out loud
+
+    the apis are in Python, and will almost certainly be on Pi, so it's just the audio that needs
+    to go somewhere
+    but there's not really a reason to have it go through the other board then?
+    the pi can just send out audio itself
+
+    TODO: parsing data from Python objects to C structures
+    this requires knowing how the depth stuff works, though
+    the depth vibration is the only part to be implemented in C
+    the CV parts are in Python and just maybe sent here
+    well, the skeleton can be written, at least
+
+    - iron out how depth vibration works again (though I don't think we're the ones writing it?)
+        - what data is needed. just depth in the direction pointed at, presumably
+        - which requires knowing which way is being pointed in... probably the center
+        - okay just give the downsampled array then
+    - is Bluetooth audio coming from Pi (Python) or the other board (C)?
+        - seemingly from the Sparkfun
+    - what format is this data coming to this program in?
+    */
+
     Py_Initialize();
     pName = PyString_FromString("realsense");
-    /* Error checking of pName left out */ //TODO:
+    if (pName == NULL)
+        return 1;
 
     pModule = PyImport_Import(pName); //TODO: not sure how to know if the top-level code executes
     Py_DECREF(pName); //DECREF = decrement ref count
@@ -83,62 +157,3 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-//not yet sure how to use this, but useful for interacting w/ numpy array
-PyObject* cos_func_np(PyObject* self, PyObject* args)
-{
-
-    PyArrayObject *in_array;
-    PyObject      *out_array;
-    NpyIter *in_iter;
-    NpyIter *out_iter;
-    NpyIter_IterNextFunc *in_iternext;
-    NpyIter_IterNextFunc *out_iternext;
-
-    /*  parse single numpy array argument */
-    if (!PyArg_ParseTuple(args, "O!", &PyArray_Type, &in_array))
-        return NULL;
-
-    /*  construct the output array, like the input array */
-    out_array = PyArray_NewLikeArray(in_array, NPY_ANYORDER, NULL, 0);
-    if (out_array == NULL)
-        return NULL;
-
-    /*  create the iterators */
-    in_iter = NpyIter_New(in_array, NPY_ITER_READONLY, NPY_KEEPORDER,
-                             NPY_NO_CASTING, NULL);
-    if (in_iter == NULL)
-        goto fail;
-
-    out_iter = NpyIter_New((PyArrayObject *)out_array, NPY_ITER_READWRITE,
-                          NPY_KEEPORDER, NPY_NO_CASTING, NULL);
-    if (out_iter == NULL) {
-        NpyIter_Deallocate(in_iter);
-        goto fail;
-    }
-
-    in_iternext = NpyIter_GetIterNext(in_iter, NULL);
-    out_iternext = NpyIter_GetIterNext(out_iter, NULL);
-    if (in_iternext == NULL || out_iternext == NULL) {
-        NpyIter_Deallocate(in_iter);
-        NpyIter_Deallocate(out_iter);
-        goto fail;
-    }
-    double ** in_dataptr = (double **) NpyIter_GetDataPtrArray(in_iter);
-    double ** out_dataptr = (double **) NpyIter_GetDataPtrArray(out_iter);
-
-    /*  iterate over the arrays */
-    do {
-        **out_dataptr = cos(**in_dataptr);
-    } while(in_iternext(in_iter) && out_iternext(out_iter));
-
-    /*  clean up and return the result */
-    NpyIter_Deallocate(in_iter);
-    NpyIter_Deallocate(out_iter);
-    Py_INCREF(out_array);
-    return out_array;
-
-    /*  in case bad things happen */
-    fail:
-        Py_XDECREF(out_array);
-        return NULL;
-}
